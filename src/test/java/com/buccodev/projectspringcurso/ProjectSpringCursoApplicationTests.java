75bb4909a0a621b90100de18a1206e1303a99e19
Fbclongobucco <longobuccofbc@gmail.com>
2023-04-24 19:08:17 -0300

    first commit

---
+++ src/test/java/com/buccodev/projectspringcurso/ProjectSpringCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buccodev.projectspringcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
