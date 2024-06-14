8ca68648472bb1a714fffa0e72ed786b1a4a6302
longobucco <longobucco>
2024-06-13 21:00:49 -0300

    reconfigure repository from orderItem.

--- src/main/java/com/buccodev/projectspringcurso/repositories/OrderItemRepository.java
+++ src/main/java/com/buccodev/projectspringcurso/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.buccodev.projectspringcurso.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.buccodev.projectspringcurso.entities.OrderItem;
+import com.buccodev.projectspringcurso.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 	
 	
